82a3825a873b5f7388e3869469863b8f84cc18f9
nikita turabov <nikitaturabov@mail.ru>
2020-03-05 11:28:11 +0300

    add project

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	//длинна строки (символы)
+	printWidth: 120,
+	//одинарные кавычки в двойные
+	singleQuote: false,
+	trailingComma: 'all',
+	//отвечает за вставку пробелов между телом объекта и фигурными скобками
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+};
