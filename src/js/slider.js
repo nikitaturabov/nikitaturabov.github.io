16b4f9634ac5e25275f9d78f7fc4ecb877f08d36
nikita turabov <nikitaturabov@mail.ru>
2020-03-29 19:17:56 +0300

    add victories section

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,7 +5,30 @@
         return;
     }
 
-    new Swiper('.swiper-container', {
+    new Swiper('.swiper-container--diplomas', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        centeredSlides: false,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {
+            click(e) {
+                if (e.target.closest(".swiper-slide__zoom-btn")) {
+
+                    zoomSlide.call(this);
+                }
+            }
+        }
+    });
+
+    new Swiper('.swiper-container--documents', {
         slidesPerView: 4,
         spaceBetween: 30,
         centeredSlides: false,
