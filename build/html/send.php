118427c3d27620f1119d74cb221a5dff0197d4c1
nikita turabov <nikitaturabov@mail.ru>
2020-04-04 14:27:04 +0300

    отправка формы на ящик

---
+++ build/html/send.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+$name = urldecode($name);
+$phone = urldecode($phone);
+$name = trim($name);
+$phone = trim($phone);
+//echo $name;
+//echo "<br>";
+//echo $phone;
+if (mail("nikitaturabov@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: zok.94@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
