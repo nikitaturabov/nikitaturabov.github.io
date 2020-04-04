6a793ba5769d724264d1a329df6491b0a4eb0292
nikita turabov <nikitaturabov@mail.ru>
2020-04-04 14:32:16 +0300

    egit php

--- build/html/send.php
+++ build/html/send.php
@@ -10,7 +10,7 @@ $phone = trim($phone);
 //echo $name;
 //echo "<br>";
 //echo $phone;
-if (mail("nikitaturabov@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: zok.94@mail.ru \r\n"))
+if (mail("46242032+nikitaturabov@users.noreply.github.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: zok.94@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
