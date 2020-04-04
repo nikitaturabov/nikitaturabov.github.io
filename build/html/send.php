657fdf491b1681e29ad3c03f5206a02123407fb8
nikita turabov <nikitaturabov@mail.ru>
2020-04-04 14:50:21 +0300

    change php

--- build/html/send.php
+++ build/html/send.php
@@ -10,8 +10,17 @@ $phone = trim($phone);
 //echo $name;
 //echo "<br>";
 //echo $phone;
-if (mail("n1kezzzzzz@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: zok.94@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+
+$to  = "<zok.94@mail.ru>, " ; 
+$to .= "zok.94@mail.ru>"; 
+
+$subject = "Заявка с лендинга"; 
+
+$message = ' <p>Текст письма</p> </br> <b>'.$phone.'</b> </br><i>'.$name.'</i> </br>';
+
+$headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
+$headers .= "From: От кого письмо <nikitaturabov@mail.ru>\r\n"; 
+$headers .= "Reply-To: reply-to@example.com\r\n"; 
+
+mail($to, $subject, $message, $headers);
+?>
